4cbea95c7d9c5ccc9c6a7235d876bf56d2c1b3e1
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-06-28 23:05:07 +0900

    Logout is added

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {auth} from '../firebase';
 import Navbar from './Navbar';
+import { useHistory } from 'react-router-dom';
 //styles
 import styles from "./Chats.module.css"
 
 const Chats = () => {
+    const history=useHistory();
+    const logoutHandler =async()=>{
+        await auth.signOut();
+        history.push("/")
+    }
     return (
         <div className={styles.container}>
-           <Navbar/> 
+           <Navbar logoutHandler={logoutHandler}/> 
         </div>
     );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from "./Navbar.module.css"
 
-const Navbar = () => {
+const Navbar = ({logoutHandler}) => {
     return (
         <div className={styles.container}>
             <div className={styles.name}>
                 Tabgram
             </div>
-            <div className={styles.logout}>
+            <div className={styles.logout} onClick={logoutHandler}>
                 Logout
             </div>
         </div>
